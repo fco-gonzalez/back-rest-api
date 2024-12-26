471e488d7b0a6ed2a5b746a28ccb05d2cd98e52c
Francisco González R <francisco@epulso.cl>
2024-12-26 17:20:18 -0300

    Configuracion de los Middleware para la aplicación

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
-import express from "express";
+import express, { application } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 export default app;
---
+++ src/middlewares/validataToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
+
+export const authRequired = (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
